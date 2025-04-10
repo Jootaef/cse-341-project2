a46cd90e9ca07ec581bb818ea77f85b4da11e1a4
jootaef <jaydanv1008@gmail.com>
2025-04-10 02:18:19 -0500

    trying to fix this

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,25 @@ const passport = require('passport');
 
 router.use('/', require('./swagger'));
 
-// router.get('/login', passport.authenticate('github'));
+router.get('/login', passport.authenticate('github'));
 
-// router.get('/auth/github/callback',
-//   passport.authenticate('github', { failureRedirect: '/login-failure' }),
-//   (req, res) => {
-//     req.session.user = req.user;
-//     req.session.save(() => {
-//       res.redirect('/login-success');
-//     });
-//   }
-// );
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/api-docs' }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  }
+);
 
-// router.get('/logout', (req, res) => {
-//   console.log('📣 Logout requested');
-  
-//   // Primero desloguear con passport
-//   req.logout((err) => {
-//     if (err) {
-//       console.error('❌ Error during logout:', err);
-//       return res.status(500).send('Error during logout');
-//     }
-    
-//     // Luego destruir la sesión
-//     req.session.destroy((err) => {
-//       if (err) {
-//         console.error('❌ Error destroying session:', err);
-//         return res.status(500).send('Error destroying session');
-//       }
-//       console.log('✅ Logout successful');
-//       res.redirect('/');
-//     });
-//   });
-// });
+router.get('/logout', function(req, res, next) {
+  req.logout(function(err){
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});  
 
 router.use('/users', require('./users'));
 router.use('/items', require('./items'));
+
 
 module.exports = router;
